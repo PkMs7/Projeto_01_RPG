32f2f421aa97721a13575ff28c663d5811ae81f6
Patrick Marques <patrickmarques2009@hotmail.com>
2022-05-19 16:21:42 -0300

    Inicio do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_01_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
