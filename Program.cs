3c68dcba8f2bbf7b226dd9f4e4e755e49efbbdba
Patrick Marques <patrickmarques2009@hotmail.com>
2022-05-19 18:09:10 -0300

    Contrução dos heróis e impressão na tela

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using Projeto_01_RPG.src.Entities;
 
 namespace Projeto_01_RPG
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Hero Arus = new Hero("Arus", 30, "Knight");
+            Hero Jessica = new Hero("Jessica", 32, "WhiteWizzard");
+            Hero Wedge = new Hero("Wedge", 29, "Ninja");
+            Hero Topapa = new Hero("Topapa", 31, "BlackWizzard");
+
+
+            Console.WriteLine(Arus);
+            Console.WriteLine(Jessica);
+            Console.WriteLine(Wedge);
+            Console.WriteLine(Topapa);
         }
     }
 }
